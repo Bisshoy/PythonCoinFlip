442bcfbd57f257a0e1737c1e091a0fd15cd7e35e
Bisshoy <42306585+Bisshoy@users.noreply.github.com>
2021-09-21 21:38:23 -0400

    Update CoinflipPython.py

--- CoinflipPython.py
+++ CoinflipPython.py
@@ -4,7 +4,7 @@ key = input("Press ENTER to flip a coin! Input anything else to quit! ")
 
 while (len(key) == 0): 
     num = random.randint(0, 1)
-    if num == 1:
+    if num == 0:
         print("HEADS!") 
         key = input("Press ENTER to flip again! Input anything else to quit! ")
     else:
